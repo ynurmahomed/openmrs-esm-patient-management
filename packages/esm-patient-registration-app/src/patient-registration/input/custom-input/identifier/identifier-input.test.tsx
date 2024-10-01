ecf8cbbd011125cdb6e8372836a1642f31069e9e
Yassin Nurmahomed <yassin.nurmahomed@csaude.org.mz>
2024-10-01 13:45:14 +0200

    (fix) Identifier input test

--- packages/esm-patient-registration-app/src/patient-registration/input/custom-input/identifier/identifier-input.test.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/input/custom-input/identifier/identifier-input.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable testing-library/no-node-access */
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Form, Formik } from 'formik';
+import React from 'react';
 import { ResourcesContext, type Resources } from '../../../../offline.resources';
 import {
   PatientRegistrationContext,
